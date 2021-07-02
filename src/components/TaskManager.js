f204f25834b53d20bfd04dbfe0f8fe3c4f3e1124
chuatuanjin <e0543708@u.nus.edu>
2021-07-03 00:00:50 +0800

    can update firebase, can display properly, can toggle properly, drop down list works but abit buggy

--- src/components/TaskManager.js
+++ src/components/TaskManager.js
@@ -1,16 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Button, TextField, Checkbox } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-// import SimpleSelect from "./SimpleSelect";
-import DatePickers from "./DatePickers";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import FormControl from "@material-ui/core/FormControl";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
 import "./Scheduler.css";
-//import Box from "../Box";
-
 import styles from "./TaskManager.module.css";
 import fire from "../fire";
 
@@ -22,67 +18,85 @@ const useStyles = makeStyles((theme) => ({
   selectEmpty: {
     marginTop: theme.spacing(2),
   },
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
 }));
 
 function TaskManager(props) {
   const { tasks, setTasks } = props; // tasks is an array of Objects
   const classes = useStyles();
   // const [newTask, setNewTask] = useState({Title:'', Module:'', dueDate: null, Type: 'Task'});
-  const [module, setModule] = React.useState('');
-  const [newTitleText, setNewTitleText] = useState('');
-  const [newDueDate, setNewDueDate] = useState('');
+  const [moduleList, setModuleList] = useState([]);
+  const [module, setModule] = React.useState("");
+  const [newTitleText, setNewTitleText] = useState("");
+  const [newDueDate, setNewDueDate] = useState(new Date());
 
-  const handleChange = (event) => {
-    console.log(event.target.value);
-    setModule(event.target.value);
-  };
-  // Our tasks and setTasks is now passed down from App
-  
+  useEffect(() => {
+    const uid = fire.auth().currentUser?.uid;
+    fire
+      .firestore()
+      .collection("Users")
+      .doc(uid)
+      .collection("Modules")
+      .doc("My Modules")
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          var modules = doc.data().modCode;
+          var help = [];
+          for (var i = 0; i < modules.length; i++) {
+            if (modules[i] !== "") {
+              help.push(modules[i]);
+            }
+          }
+          setModuleList(help);
+        } else {
+        }
+      });
+  }, []);
 
   function handleAddTask(event) {
     // React honours default browser behavior and the
     // default behaviour for a form submission is to
     // submit AND refresh the page. So we override the
     // default behaviour here as we don't want to refresh
-    console.log(event);
+    // console.log(event);
+    const date = new Date(newDueDate);
     event.preventDefault();
-    const task = {Title:newTitleText, Module:module, dueData: newDueDate, Type:'Task',isComplete: false}
+    const task = {
+      Title: newTitleText,
+      Module: module,
+      dueDate: date,
+      dueDateString: date.toString(),
+      Type: "Task",
+      isComplete: false,
+    };
     // setNewTask(task);
-    // console.log(newTask);
+    console.log(task);
     addTask(task, fire);
   }
 
   function addTask(task) {
-    console.log(task);
     const newTasks = [
-      // the ... operator is called the spread operator
-      // what we are doing is creating a brand new array of
-      // tasks, that is different from the previous array
-      // See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
       ...tasks,
       {
         ...task,
       },
     ];
-    setTasks(newTasks);
-    console.log(newTasks);
-  }
+    setTasks(newTasks); // This works: tasks will be updated to newTasks
+    function GuidFun() {
+      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    }
 
-  // function handleChange(event) {
-  //   const value = event.target.value;
-  //   setNewTask({...newTask, [event.target.name]: value});
-  // }
-
-  function GuidFun() {
-    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-  }
-
-  useEffect(() => {
-      const uid = fire.auth().currentUser?.uid;
-      const db = fire.firestore();
-      db.collection("Users")
-    .doc(uid)
-    .collection("Tasks").doc((
+    function guid() {
+      return (
         GuidFun() +
         GuidFun() +
         "-" +
@@ -95,13 +109,33 @@ function TaskManager(props) {
         GuidFun() +
         GuidFun() +
         GuidFun()
-      ).toLowerCase()).set({ tasks });
-    }, [tasks]);
+      ).toLowerCase();
+    }
+    const uid = fire.auth().currentUser?.uid;
+    const db = fire.firestore();
+    const date = task.dueDate;
+    const dateString = date.toString();
+    const DocumentId = guid();
+    db.collection("Users").doc(uid).collection("Tasks").doc(DocumentId).set({
+      DocumentId: DocumentId,
+      Title: task.Title,
+      Module: task.Module,
+      dueDate: date,
+      dueDateString: dateString,
+      Type: task.Type,
+      isComplete: task.isComplete,
+    });
+  }
+
+  // function handleChange(event) {
+  //   const value = event.target.value;
+  //   setNewTask({...newTask, [event.target.name]: value});
+  // }
 
   return (
     <>
       <h2>Add Tasks</h2>
-      
+      {/* <FormControl className={classes.formControl} onSubmit={handleAddTask}> */}
       <form className={styles.addTaskForm} onSubmit={handleAddTask}>
         <TextField
           className={styles.descTextField}
@@ -109,23 +143,49 @@ function TaskManager(props) {
           value={newTitleText}
           onChange={(event) => setNewTitleText(event.target.value)}
         />
-        <InputLabel id="demo-simple-select-label">Module</InputLabel>
+        <InputLabel id="demo-simple-select-outlined-label">Module</InputLabel>
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          value={module}
+          label="Module"
+        >
+          {moduleList.map((mod) => (
+            <MenuItem key={mod} onClick={(event) => setModule(mod)}>
+              {mod}
+            </MenuItem>
+          ))}
+        </Select>
+        {/* <InputLabel id="demo-simple-select-label">Module</InputLabel>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           value={module}
-          onChange={handleChange}
-        ><MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-        {/* <DatePickers
-          value={newTask.dueDate}
-          onChange={(event) => setNewTask(event.target.value)}
-        /> */}
+          onChange={(event) => setModule(event.target.value)}
+        >
+          <MenuItem value={"CS1010S"}>CS1010S</MenuItem>
+          <MenuItem value={"MA1521"}>MA1521</MenuItem>
+          <MenuItem value={"IS1103"}>IS1103</MenuItem>
+        </Select> */}
+        <TextField
+          id="date"
+          label="Due Date"
+          type="datetime-local"
+          className={classes.textField}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          value={newDueDate}
+          onChange={(event) => {
+            setNewDueDate(event.target.value);
+            console.log(event.target.value);
+            console.log(newDueDate);
+          }}
+        />
         <Button type="submit" variant="contained" color="primary">
           Add
         </Button>
+        {/* </FormControl> */}
       </form>
       <h2>Task List</h2>
       {tasks.length > 0 ? (
@@ -139,13 +199,14 @@ function TaskManager(props) {
 
 function TaskList(props) {
   const { tasks, setTasks } = props;
-
-  function handleTaskCompletionToggled(toToggleTask, toToggleTaskIndex) {
+  function handleTaskCompletionToggled(toToggleTask, toToggleTaskIndex, event) {
+    console.log(event);
+    event.preventDefault();
+    console.log(toToggleTask);
     const newTasks = [
-      // Once again, this is the spread operator
       ...tasks.slice(0, toToggleTaskIndex),
       {
-        description: toToggleTask.description,
+        ...toToggleTask,
         isComplete: !toToggleTask.isComplete,
       },
       ...tasks.slice(toToggleTaskIndex + 1),
@@ -153,8 +214,23 @@ function TaskList(props) {
     // We set new tasks in such a complex way so that we maintain immutability
     // Read this article to find out more:
     // https://blog.logrocket.com/immutability-in-react-ebe55253a1cc/
-
     setTasks(newTasks);
+    const uid = fire.auth().currentUser?.uid;
+    const db = fire.firestore();
+    console.log(!toToggleTask.isComplete);
+    const boolean = !toToggleTask.isComplete;
+    db.collection("Users")
+      .doc(uid)
+      .collection("Tasks")
+      .doc(toToggleTask.DocumentId)
+      .update({
+        isComplete: boolean,
+      });
+    const docRef = db.collection("Users").doc(uid).collection("Tasks");
+    docRef.get().then((querySnapshot) => {
+      const data = querySnapshot.docs.map((doc) => doc.data());
+      setTasks(data);
+    });
   }
 
   return (
@@ -163,6 +239,7 @@ function TaskList(props) {
         <tr>
           <th>No.</th>
           <th>Task</th>
+          <th>Due Date</th>
           <th>Completed</th>
         </tr>
       </thead>
@@ -171,14 +248,17 @@ function TaskList(props) {
           // We should specify key here to help react identify
           // what has updated
           // https://reactjs.org/docs/lists-and-keys.html#keys
-          <tr key={task.description}>
+          <tr key={index}>
             <td>{index + 1}</td>
-            <td>{task.description}</td>
+            <td>{task.Title}</td>
+            <td>{task.dueDateString}</td>
             <td>
               <Checkbox
                 color="primary"
                 checked={task.isComplete}
-                onChange={() => handleTaskCompletionToggled(task, index)}
+                onChange={(event) =>
+                  handleTaskCompletionToggled(task, index, event)
+                }
                 inputProps={{
                   "aria-label": `checkbox that determines if task ${index} is done`,
                 }}
